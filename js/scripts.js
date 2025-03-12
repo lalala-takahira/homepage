8dc338346c1edc80d53c65870821143254c05b3f
unknown <bask022427@gmail.com>
2025-03-13 00:10:28 +0900

    スタイル変更

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,37 @@
-// scripts.js
 $(document).ready(function() {
     console.log('Document is ready');
-   // メニューボタンのクリックイベント
+  
+    // タイトル部分クリック時に開閉する処理
+    $('.report-summary').on('click', function() {
+      // クリックされたsummaryのdata-idを取得
+      let reportId = $(this).attr('data-id');
+      
+      // すでに開いてるものがあれば閉じる（.open を外す＆slideUp）
+      $('.report-summary.open').not(this).removeClass('open');
+      $('.report-detail:visible').not('#'+reportId).slideUp();
+  
+      // 自分が開いているかどうか判定
+      if ($(this).hasClass('open')) {
+        // 自分がopenだった場合は閉じる
+        $(this).removeClass('open');
+        $('#' + reportId).slideUp();
+      } else {
+        // 自分が閉じている場合は開く
+        $(this).addClass('open');
+        $('#' + reportId).slideDown();
+      }
+    });
+  
+    // メニュー開閉（既存のコードがある場合はここに）
     $('#menu-button').click(function() {
-        console.log('Menu button clicked');
-        $('#nav-menu').toggleClass('active');
+      $('#nav-menu').toggleClass('active');
     });
-
-    // ウィンドウリサイズイベント
+  
+    // ウィンドウリサイズ時にメニューを閉じる等
     $(window).resize(function() {
-        if ($(window).width() > 768) {
-            $('#nav-menu').removeClass('active');
-        }
+      if ($(window).width() > 768) {
+        $('#nav-menu').removeClass('active');
+      }
     });
-});
+  });
+  
